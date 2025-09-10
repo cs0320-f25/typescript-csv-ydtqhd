e7e8ea6d1449678e9bbe1e87ba7ac7e7ae03b71e
Eddie Lin <hancheng_lin@brown.edu>
2025-09-10 16:24:15 -0400

    chore: change indent from 2 spaces to 4

--- src/run-parser.ts
+++ src/run-parser.ts
@@ -7,15 +7,15 @@ import { parseCSV } from "./basic-parser";
 const DATA_FILE = "./data/people.csv"; // update with your actual file name
 
 async function main() {
-  // Because the parseCSV function needs to "await" data, we need to do the same here.
-  const results = await parseCSV(DATA_FILE)
+    // Because the parseCSV function needs to "await" data, we need to do the same here.
+    const results = await parseCSV(DATA_FILE)
 
-  // Notice the difference between "of" and "in". One iterates over the entries, 
-  // another iterates over the indexes only.
-  for(const record of results)
-    console.log(record)
-  for(const record in results)
-    console.log(record)
+    // Notice the difference between "of" and "in". One iterates over the entries, 
+    // another iterates over the indexes only.
+    for (const record of results)
+        console.log(record)
+    for (const record in results)
+        console.log(record)
 }
 
 main();
