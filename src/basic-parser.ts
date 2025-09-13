0375597831bd23299dd3ff69d0f1f17e75d1338c
Eddie Lin <hancheng_lin@brown.edu>
2025-09-13 04:20:26 -0400

    feat: implement zod schema support and hasHeader option

--- src/basic-parser.ts
+++ src/basic-parser.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as readline from "readline";
+import * as z from "zod";
 
 /**
  * This is a JSDoc comment. Similar to JavaDoc, it documents a public-facing
@@ -12,9 +13,11 @@ import * as readline from "readline";
  * You shouldn't need to alter them.
  * 
  * @param path The path to the file being loaded.
+ * @param schema The Zod schema used on parsing data rows.
+ * @param hasHeader Omit the first row if provided with `true`.
  * @returns a "promise" to produce a 2-d array of cell values
  */
-export async function parseCSV(path: string): Promise<string[][]> {
+export async function parseCSV<T>(path: string, schema?: z.ZodType<T>, hasHeader?: boolean): Promise<(string[] | T)[]> {
     // This initial block of code reads from a file in Node.js. The "rl"
     // value can be iterated over in a "for" loop. 
     const fileStream = fs.createReadStream(path);
@@ -31,7 +34,16 @@ export async function parseCSV(path: string): Promise<string[][]> {
     // More on this in class soon!
     for await (const line of rl) {
         const values = line.split(",").map((v) => v.trim());
-        result.push(values)
+        if (hasHeader) {
+            hasHeader = false;
+            continue;
+        }
+
+        if (schema) {
+            result.push(schema.parse(values))
+        } else {
+            result.push(values)
+        }
     }
     return result
 }
